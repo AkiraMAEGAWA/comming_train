9f0a16b794ca99e0da95bae4aa1dee2ca7fac694
AkiraMAEGAWA <maeaki112@gmail.com>
2016-10-15 02:03:10 +0900

    initial commit
    簡単に駅を作った

---
+++ coming_train/Assets/ChangeDirection.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeDirection : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey ("up")) {
+			transform.position += transform.forward * 0.01f;
+		}
+		if (Input.GetKey ("right")){
+			transform.Rotate (0, 1, 0);
+	    }
+        if (Input.GetKey ("left")){ 
+    		transform.Rotate (0, -1, 0);
+        }
+	}
+}
---
+++ coming_train/Assets/PassAway.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PassAway : MonoBehaviour {
+
+	private Vector3 m_pos;
+	// Use this for initialization
+	void Start () {
+		m_pos = transform.localPosition; 
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.localPosition = m_pos;
+		m_pos.x += 0.05f;
+	}
+}
